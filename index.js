60178832c0d35e8f2c9d9d35038e9d691dd2b2d6
Taras Sheremet <tsheremet@basqauare.com>
2017-08-14 01:33:10 +0300

    Modify code according to standart

--- index.js
+++ index.js
@@ -1,84 +1,84 @@
-var Instagram = require('./src/instagram.js');
-var Promice = require('bluebird');
+var Instagram = require('./src/instagram.js')
+var Promice = require('bluebird')
 
 // Function to prapare request task with social network options and request function
-function createTask(option, func) {
-    return func(option).catch((ex) => {
+function createTask (option, func) {
+  return func(option).catch((ex) => {
         // Made because promice all is used. not to stop processing for other possible social networks
-        return Promice.resolve({});
-    });
+    return Promice.resolve({})
+  })
 }
 
 // Function to get user locations info from instagram and format to universal model
-function instagramRecent(option) {
-    return Instagram.getUserRecent(option.user, null)
+function instagramRecent (option) {
+  return Instagram.getUserRecent(option.user, null)
         .then((feed) => {
-            var feed_data = feed.data;
-            var userLocations = [];
-            for (var index in feed_data) {
-                var feed_record = feed_data[index];
-                if (feed_record.location) {
-                    var location_item = {
-                        record_id: feed_record.id,
-                        latitude: feed_record.location.latitude,
-                        longitude: feed_record.location.longitude,
-                        timedate: new Date(parseInt(feed_record.created_time) * 1000),
-                        users_tagged: []
-                    };
-                    for (var user_index in feed_record.users_in_photo) {
-                        var user_in_photo = feed_record.users_in_photo[user_index];
-                        location_item.users_tagged.push({
-                            provider: 'instagram',
-                            id: user_in_photo.user.id,
-                            name: user_in_photo.user.full_name
-                        });
-                    }
-                    userLocations.push(location_item);
-                }
+          var feedData = feed.data
+          var userLocations = []
+          for (var index in feedData) {
+            var feedRecord = feedData[index]
+            if (feedRecord.location) {
+              var locationItem = {
+                record_id: feedRecord.id,
+                latitude: feedRecord.location.latitude,
+                longitude: feedRecord.location.longitude,
+                timedate: new Date(parseInt(feedRecord.created_time) * 1000),
+                usersTagged: []
+              }
+              for (var userIndex in feedRecord.usersOnPhoto) {
+                var userOnPhoto = feedRecord.usersOnPhoto[userIndex]
+                locationItem.usersTagged.push({
+                  provider: 'instagram',
+                  id: userOnPhoto.user.id,
+                  name: userOnPhoto.user.fullName
+                })
+              }
+              userLocations.push(locationItem)
             }
-            return { instagram: userLocations };
-        });
+          }
+          return { instagram: userLocations }
+        })
 }
 
 // List of providers, will be used to collecting info
 const socials = {
-    instagram:instagramRecent
-};
+  instagram: instagramRecent
+}
 
-var social_location = function (config) {
-    var self = this;
-    self.config = config || {}; // general config for future use
+var socialLocation = function (config) {
+  var self = this
+  self.config = config || {} // general config for future use
 
-    self.recentLocation = function (option) {
-        var tasks = [];
+  self.recentLocation = function (option) {
+    var tasks = []
         // Preparing tasks according to options.
         // Options should be an object with following structure:
         // { "social1":{/* social1 request options */}, "social2":{/* social2 request options */} }
-        for(var key in socials) {
-            if(option[key]) {
-                var requestOption = {};
-                requestOption.user = option[key];
-                requestOption.client = self.config[key];
-                tasks.push(createTask(requestOption, socials[key]));
-            }
-        }
+    for (var key in socials) {
+      if (option[key]) {
+        var requestOption = {}
+        requestOption.user = option[key]
+        requestOption.client = self.config[key]
+        tasks.push(createTask(requestOption, socials[key]))
+      }
+    }
         // Waiting until all tasks done
-        return Promice.all(tasks)
-            .then((promices_result) => {
-                var result = {};
-                for (var promice_index in promices_result) {
-                    for (var provider_index in promices_result[promice_index]) {
-                        result[provider_index] = promices_result[promice_index][provider_index];
-                    }
+    return Promice.all(tasks)
+            .then((promicesResult) => {
+              var result = {}
+              for (var promiceIndex in promicesResult) {
+                for (var providerIndex in promicesResult[promiceIndex]) {
+                  result[providerIndex] = promicesResult[promiceIndex][providerIndex]
                 }
-                return result;
+              }
+              return result
             })
             .catch((ex) => {
-                return { error:ex };
-            });
-    };
+              return { error: ex }
+            })
+  }
 
-    return self;
+  return self
 }
 
-module.exports = social_location;
+module.exports = socialLocation
--- src/instagram.js
+++ src/instagram.js
@@ -1,32 +1,32 @@
-const querystring = require('querystring');
-const util = require('util');
-const bluebird = require('bluebird');
-const request = bluebird.promisifyAll(require('request'));
+const querystring = require('querystring')
+const util = require('util')
+const bluebird = require('bluebird')
+const request = bluebird.promisifyAll(require('request'))
 
-const tokenPattern = "?access_token=%s"
-const apiBaseURL = "https://api.instagram.com/v1";
-const recent = "/users/%s/media/recent"
+const tokenPattern = '?access_token=%s'
+const apiBaseURL = 'https://api.instagram.com/v1'
+const recent = '/users/%s/media/recent'
 
 var instagramHelper = {}
 instagramHelper.getUserRecent = function (token, user, options) {
-    if (!user) user = "self";
-    var url = apiBaseURL;
-    url += util.format(recent, user);
-    url += util.format(tokenPattern, token);
-    if (options) {
-        url += "&";
-        url += querystring.stringify(options);
-    }
-    return request.getAsync(url)
+  if (!user) user = 'self'
+  var url = apiBaseURL
+  url += util.format(recent, user)
+  url += util.format(tokenPattern, token)
+  if (options) {
+    url += '&'
+    url += querystring.stringify(options)
+  }
+  return request.getAsync(url)
         .then((res) => {
-            var obj;
-            try {
-                obj = JSON.parse(res.body);
-            } catch (ex) {
-                obj = null;
-            }
-            return obj;
-        });
+          var obj
+          try {
+            obj = JSON.parse(res.body)
+          } catch (ex) {
+            obj = null
+          }
+          return obj
+        })
 }
 
-module.exports = instagramHelper;
+module.exports = instagramHelper

efa12f26e02b16437368924c09705d6fcab63b2b
Sylvain Conso <sylvain.flaneur@gmail.com>
2018-04-04 19:27:49 +0200

    Added Facebook API

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const InstagramAPI = require('./services/instagram')
+const FacebookAPI = require('./services/facebook')
 
 // Expose API
 exports.instagramAPI = InstagramAPI
+exports.facebookAPI = FacebookAPI
